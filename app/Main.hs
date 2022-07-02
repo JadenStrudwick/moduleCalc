b0d1816e244c606ee0e846d3769dd85a65d261cf
Jaden <strudjay@gmail.com>
2022-07-02 23:53:30 +0100

    Pedantic Fixes

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,6 @@ module Main where
 
 import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
-import Graphics.UI.Threepenny.Widgets
-
-import CLI ( cliMain )
-import Control.Monad (void)
 
 main :: IO ()
 main = do
@@ -15,8 +11,8 @@ main = do
 
 setup :: Window -> UI ()
 setup window = do
-    return window # set title "Module Calculator"
-    mkReactiveTextField window "test"
+    _ <- return window # set title "Module Calculator"
+    _ <- mkReactiveTextField window "test"
     return ()
 
 mkReactiveTextField :: Window -> String -> UI Element
@@ -24,17 +20,17 @@ mkReactiveTextField window labelString = do
 
     label <- UI.p # set text labelString                -- Create paragraph label
     input <- UI.input # set value ""                    -- Create input field
-    div <- UI.div #+ [element label, element input]     -- Create div container
-    getBody window #+ [element div]                     -- Add div to body
+    divBox <- UI.div #+ [element label, element input]  -- Create div container
+    _ <- getBody window #+ [element divBox]             -- Add div to body
 
     on UI.keyup input $ \keyCode -> do                  -- When input field is changed
         if keyCode == 13 then do                        -- If enter key is pressed
             val <- get value input                      -- Get value of input field
-            div' <- mkReactiveTextField window val      -- Create new div
-            delete div                                  -- Delete old div
-            return div'                                 -- Return new div
+            divBox' <- mkReactiveTextField window val   -- Create new div
+            delete divBox                               -- Delete old div
+            return divBox'                              -- Return new div
         else 
-            return div                                  -- Return div container 
+            return divBox                               -- Return div container 
 
-    return div                                          -- Return div container
+    return divBox                                       -- Return div container
 
