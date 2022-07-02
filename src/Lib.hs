53032ded036379317f053851baf8a8984dbb98c0
Jaden <strudjay@gmail.com>
2022-07-02 19:28:43 +0100

    Setup Testing Suite

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,27 +15,33 @@ makeCoursework = Coursework
 
 -- | Module data type
 data Module = Module {
-    moduleName      :: String,
-    courseworks     :: [Coursework],
-    moduleWeight    :: Float,
-    moduleMark      :: Float
+    mName      :: String,
+    mCws       :: [Coursework],
+    mWeight    :: Float,
+    mMark      :: Float
 }
 
 instance Show Module where
-    show m = "Module name: " ++ moduleName m ++ " (" ++ show (moduleWeight m) ++ "% weight): " ++ " \n" ++ unlines (map show (courseworks m) )++ " Module percent: " ++ show (moduleMark m) ++ "% \n"
+    show m = "Module name: " ++ mName m ++ " (" ++ show (mWeight m) ++ "% weight): " ++ " \n" ++ unlines (map show (mCws m) )++ " Module percent: " ++ show (mMark m) ++ "% \n"
 
+-- | Smart constructor for Module data type
+-- Empty coursework list creates a module with 0 weight and 0 mark
 makeModule :: String -> [Coursework] -> Float -> Module
+makeModule name [] weight = Module name [] 0 0
 makeModule name cws weight = Module name cws weight (sum [ (cwWeight cw / 100) * cwMark cw | cw <- cws])
 
 -- | Year data type
 data Year = Year {
-    modules     :: [Module],
-    yearWeight  :: Float,
-    yearMark    :: Float
+    yMs         :: [Module],
+    yWeight     :: Float,
+    yMark       :: Float
 }
 
 instance Show Year where
-    show y = unlines (map show (modules y)) ++ "Year percent: (" ++ show (yearWeight y) ++ "% weight): " ++ show (yearMark y) ++ "% \n"
+    show y = unlines (map show (yMs y)) ++ "Year percent (" ++ show (yWeight y) ++ "% weight): " ++ show (yMark y) ++ "% \n"
 
+-- | Smart constructor for Year data type
+-- Empty module list creates a year with 0 weight and 0 mark
 makeYear :: [Module] -> Float -> Year
-makeYear ms weight = Year ms weight (sum [moduleMark m | m <- ms] / fromIntegral (length ms))
+makeYear [] weight = Year [] 0 0
+makeYear ms weight = Year ms weight (sum [ mMark m | m <- ms] / fromIntegral (length ms))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,41 @@
+import Test.QuickCheck
+import Lib 
+
+instance Arbitrary Coursework where
+    arbitrary = do
+        name <- arbitrary
+        weight <- arbitrary
+        mark <- arbitrary
+        return $ makeCoursework name weight mark
+
+instance Arbitrary Module where
+    arbitrary = do
+        name <- arbitrary
+        cws <- arbitrary
+        weight <- arbitrary
+        return $ makeModule name cws weight
+
+instance Arbitrary Year where
+    arbitrary = do
+        modules <- arbitrary
+        weight <- arbitrary
+        return $ makeYear modules weight
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    quickCheck prop_makeModule
+    quickCheck prop_makeYear
+    putStrLn "Done"
+
+prop_makeModule :: String -> [Coursework] -> Float -> Bool
+prop_makeModule name cws weight = mMark m == sum [ (cwWeight cw / 100) * cwMark cw | cw <- cws]
+    where
+        m = makeModule name cws weight
+
+prop_makeYear :: [Module] -> Float -> Bool
+prop_makeYear [] weight = yMark y == 0
+    where
+        y = makeYear [] weight
+prop_makeYear ms weight = yMark y == sum [ mMark m | m <- ms] / fromIntegral (length ms)
+    where
+        y = makeYear ms weight
